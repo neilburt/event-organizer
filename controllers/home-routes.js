bc70b7b9d53d08bb59431409063abd0bb07c1706
Blenda Orellana <blen.or90@gmail.com>
2021-07-23 14:33:38 -0500

    added event files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 // const withAuth = require('../utils/auth');
 const { User } = require("../models");
-
+// const fetch = require("node-fetch");
 
 router.get('/', async (req, res) => {
   console.log("Home page");
@@ -26,6 +26,34 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/event', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('event');
+});
+
+
+// router.get('/apitest', async function(req, res) {
+//     console.log("api hit")
+  
+//     // const apikey = "41ea6b300379c4e9d39458d185e60c52b49d4768398589851fa2ecdd1bd7b30f:MjI2MjI4NTh8MTYyNjk3MDg5NS41Mzc3NTc";
+  
+//     const clientId = "MjI2MjI4NTh8MTYyNjk3MDg5NS41Mzc3NTc";
+//     const clientSecret = "41ea6b300379c4e9d39458d185e60c52b49d4768398589851fa2ecdd1bd7b30f";
+//     const query = "events";
+//     const id = "2";
+  
+//     const result = await fetch(`https://api.seatgeek.com/${id}/${query}?client_id=${clientId}&client_secret=${clientSecret}`);
+//     const data = await result.json()
+  
+//     console.log(data)
+  
+//     res.json({data: data});
+  
+//   });
 
 
 
