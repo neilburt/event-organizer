ae3b541852d15e7af002cb06fb52c8336f463e0d
neilburt <neil.burt@comcast.net>
2021-07-27 09:58:16 -0500

    Gary signup fix and zipcode add.

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 const hbs = exphbs.create({});
 
 const sess = {
@@ -27,9 +31,7 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(routes);
 
